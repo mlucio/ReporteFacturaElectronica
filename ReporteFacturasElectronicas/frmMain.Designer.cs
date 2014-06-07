38799709053dedaa927e4820b4692773e24c999c
Marco Lucio <mlucio@gmail.com>
2014-06-07 18:55:56 -0500

    Primera carga

---
+++ ReporteFacturasElectronicas/frmMain.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace ReporteFacturasElectronicas
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.seleccionaArchivo = new System.Windows.Forms.Button();
+            this.procesaArchivo = new System.Windows.Forms.Button();
+            this.tbArchivoPDF = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.FolioFiscal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.RFCEmisor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.RazonSocialEmisor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.RFCReceptor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.RazonSocialReceptor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PACCertifico = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Total = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FechaEmision = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FechaCertificacion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.EstadoComprobante = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.EfectoComprobante = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tbArchivoGenerar = new System.Windows.Forms.TextBox();
+            this.generarArchivo = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // seleccionaArchivo
+            // 
+            this.seleccionaArchivo.Location = new System.Drawing.Point(1026, 32);
+            this.seleccionaArchivo.Name = "seleccionaArchivo";
+            this.seleccionaArchivo.Size = new System.Drawing.Size(75, 23);
+            this.seleccionaArchivo.TabIndex = 0;
+            this.seleccionaArchivo.Text = "Seleccionar Archivo";
+            this.seleccionaArchivo.UseVisualStyleBackColor = true;
+            this.seleccionaArchivo.Click += new System.EventHandler(this.seleccionaArchivo_Click);
+            // 
+            // procesaArchivo
+            // 
+            this.procesaArchivo.Location = new System.Drawing.Point(1113, 33);
+            this.procesaArchivo.Name = "procesaArchivo";
+            this.procesaArchivo.Size = new System.Drawing.Size(75, 23);
+            this.procesaArchivo.TabIndex = 1;
+            this.procesaArchivo.Text = "Procesar";
+            this.procesaArchivo.UseVisualStyleBackColor = true;
+            this.procesaArchivo.Click += new System.EventHandler(this.procesaArchivo_Click);
+            // 
+            // tbArchivoPDF
+            // 
+            this.tbArchivoPDF.Location = new System.Drawing.Point(25, 35);
+            this.tbArchivoPDF.Name = "tbArchivoPDF";
+            this.tbArchivoPDF.Size = new System.Drawing.Size(986, 20);
+            this.tbArchivoPDF.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.FolioFiscal,
+            this.RFCEmisor,
+            this.RazonSocialEmisor,
+            this.RFCReceptor,
+            this.RazonSocialReceptor,
+            this.PACCertifico,
+            this.Total,
+            this.FechaEmision,
+            this.FechaCertificacion,
+            this.EstadoComprobante,
+            this.EfectoComprobante});
+            this.dataGridView1.Location = new System.Drawing.Point(25, 71);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1163, 150);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // FolioFiscal
+            // 
+            this.FolioFiscal.HeaderText = "Folio Fiscal";
+            this.FolioFiscal.Name = "FolioFiscal";
+            // 
+            // RFCEmisor
+            // 
+            this.RFCEmisor.HeaderText = "RFC Emisor";
+            this.RFCEmisor.Name = "RFCEmisor";
+            // 
+            // RazonSocialEmisor
+            // 
+            this.RazonSocialEmisor.HeaderText = "Razón Social Emisor";
+            this.RazonSocialEmisor.Name = "RazonSocialEmisor";
+            // 
+            // RFCReceptor
+            // 
+            this.RFCReceptor.HeaderText = "RFC Receptor";
+            this.RFCReceptor.Name = "RFCReceptor";
+            // 
+            // RazonSocialReceptor
+            // 
+            this.RazonSocialReceptor.HeaderText = "Razón Social Receptor";
+            this.RazonSocialReceptor.Name = "RazonSocialReceptor";
+            // 
+            // PACCertifico
+            // 
+            this.PACCertifico.HeaderText = "PAC que Certificó";
+            this.PACCertifico.Name = "PACCertifico";
+            // 
+            // Total
+            // 
+            this.Total.HeaderText = "Total";
+            this.Total.Name = "Total";
+            // 
+            // FechaEmision
+            // 
+            this.FechaEmision.HeaderText = "Fecha Emisión";
+            this.FechaEmision.Name = "FechaEmision";
+            // 
+            // FechaCertificacion
+            // 
+            this.FechaCertificacion.HeaderText = "Fecha Certificación";
+            this.FechaCertificacion.Name = "FechaCertificacion";
+            // 
+            // EstadoComprobante
+            // 
+            this.EstadoComprobante.HeaderText = "Estado del Comprobante";
+            this.EstadoComprobante.Name = "EstadoComprobante";
+            // 
+            // EfectoComprobante
+            // 
+            this.EfectoComprobante.HeaderText = "Efecto del Comprobante";
+            this.EfectoComprobante.Name = "EfectoComprobante";
+            // 
+            // tbArchivoGenerar
+            // 
+            this.tbArchivoGenerar.Location = new System.Drawing.Point(25, 245);
+            this.tbArchivoGenerar.Name = "tbArchivoGenerar";
+            this.tbArchivoGenerar.Size = new System.Drawing.Size(986, 20);
+            this.tbArchivoGenerar.TabIndex = 4;
+            // 
+            // generarArchivo
+            // 
+            this.generarArchivo.Location = new System.Drawing.Point(1026, 243);
+            this.generarArchivo.Name = "generarArchivo";
+            this.generarArchivo.Size = new System.Drawing.Size(117, 23);
+            this.generarArchivo.TabIndex = 5;
+            this.generarArchivo.Text = "Generar Archivo";
+            this.generarArchivo.UseVisualStyleBackColor = true;
+            this.generarArchivo.Click += new System.EventHandler(this.generarArchivo_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 293);
+            this.Controls.Add(this.generarArchivo);
+            this.Controls.Add(this.tbArchivoGenerar);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.tbArchivoPDF);
+            this.Controls.Add(this.procesaArchivo);
+            this.Controls.Add(this.seleccionaArchivo);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "Reporte de Facturas Electrónicas";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button seleccionaArchivo;
+        private System.Windows.Forms.Button procesaArchivo;
+        private System.Windows.Forms.TextBox tbArchivoPDF;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FolioFiscal;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RFCEmisor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RazonSocialEmisor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RFCReceptor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RazonSocialReceptor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PACCertifico;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Total;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FechaEmision;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FechaCertificacion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn EstadoComprobante;
+        private System.Windows.Forms.DataGridViewTextBoxColumn EfectoComprobante;
+        private System.Windows.Forms.TextBox tbArchivoGenerar;
+        private System.Windows.Forms.Button generarArchivo;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ ReporteFacturasElectronicas/frmMain.cs
@@ -0,0 +1,144 @@
+﻿/*
+ * Maco Antonio Lucio Rocha
+ * 07JUN2014
+ * Pograma para pasar el resultado de facturas electrónicas de hacienda a texto CSV,
+ * para poder hacer reportes de este.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System.Text.RegularExpressions;
+
+namespace ReporteFacturasElectronicas
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void seleccionaArchivo_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "pdf files (*.pdf)|*.pdf|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if (openFileDialog1.CheckFileExists)
+                    {
+                        tbArchivoPDF.Text = openFileDialog1.FileName;
+                        tbArchivoGenerar.Text = Path.ChangeExtension(tbArchivoPDF.Text, ".csv");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: No se puede leer el archivo. Error original:" + ex.Message);
+                }
+            }
+        }
+
+        private void procesaArchivo_Click(object sender, EventArgs e)
+        {
+            if(tbArchivoPDF.Text != "" && File.Exists(tbArchivoPDF.Text))
+            {
+                ExtraerTextoDePDF(tbArchivoPDF.Text);    
+            }
+        }
+
+        public void ExtraerTextoDePDF(string pdfFile)
+        {
+            PdfReader reader = new PdfReader(pdfFile);
+            int numeroDePaginas;
+            string text = "";
+
+            numeroDePaginas = reader.NumberOfPages;
+            for (int i = 0; i < numeroDePaginas; i++)
+            {
+                text += PdfTextExtractor.GetTextFromPage(reader, i + 1);
+            }
+            try { reader.Close(); }
+            catch { }
+            //text = text.Replace(System.Environment.NewLine, " ");
+            //text = text.Replace("\n", " ");
+            Regex regex = new Regex(@"Folio Fiscal: (?<FolioFiscal>[A-Z 0-9 \-]*)\s*" +
+                @"RFC Emisor: (?<RFCEmisor>[A-Z 0-9]+)\s+" +
+                @"Nombre o Razón Social: (?<RazonSocialEmisor>.*\n*.*)\s+" +
+                @"RFC Receptor: (?<RFCReceptor>[A-Z 0-9]+)\s+" +
+                @"Nombre o Razón Social: (?<RazonSocialReceptor>.*\n*.*)\s+" +
+                @"PAC que Certificó: (?<PACCertifico>[A-Z 0-9]*)\s*" +
+                @"Total: \$(?<Total>[0-9 , \.]*)\s*" +
+                @"Fecha Emisión: (?<FechaEmision>[0-9 T \- \:]*)\s*" +
+                @"Fecha Certificación: (?<FechaCertificacion>[0-9 T \- \:]*)\s*" +
+                @"Estado del Comprobante: (?<EstadoComprobante>[A-Z a-z]*)\s*" +
+                @"Efecto del Comprobante: (?<EfectoComprobante>[A-Z a-z]*)\s*");
+            MatchCollection matches = regex.Matches(text);
+            int j = 0;
+            foreach (Match match in matches)
+            {
+                dataGridView1.Rows.Add();
+                dataGridView1.Rows[j].Cells[0].Value = match.Groups["FolioFiscal"].Value;
+                dataGridView1.Rows[j].Cells[1].Value = match.Groups["RFCEmisor"].Value;
+                dataGridView1.Rows[j].Cells[2].Value = match.Groups["RazonSocialEmisor"].Value.Replace("\n","");
+                dataGridView1.Rows[j].Cells[3].Value = match.Groups["RFCReceptor"].Value;
+                dataGridView1.Rows[j].Cells[4].Value = match.Groups["RazonSocialReceptor"].Value.Replace("\n","");
+                dataGridView1.Rows[j].Cells[5].Value = match.Groups["PACCertifico"].Value;
+                dataGridView1.Rows[j].Cells[6].Value = match.Groups["Total"].Value;
+                dataGridView1.Rows[j].Cells[7].Value = match.Groups["FechaEmision"].Value;
+                dataGridView1.Rows[j].Cells[8].Value = match.Groups["FechaCertificacion"].Value;
+                dataGridView1.Rows[j].Cells[9].Value = match.Groups["EstadoComprobante"].Value;
+                dataGridView1.Rows[j].Cells[10].Value = match.Groups["EfectoComprobante"].Value;
+                j++;
+            }
+
+        }
+
+        private void generarArchivo_Click(object sender, EventArgs e)
+        {
+            System.IO.StreamWriter objWriter;
+
+            if(tbArchivoGenerar.Text == "")
+            {
+                if (saveFileDialog1.ShowDialog() != DialogResult.OK)
+                {
+                    MessageBox.Show("No se seleccionó un archivo.");                    
+                }
+                else
+                {
+                    tbArchivoGenerar.Text = saveFileDialog1.FileName;
+                }
+            }
+            if(tbArchivoGenerar.Text != "" && dataGridView1.RowCount > 0)
+            {
+                objWriter = new System.IO.StreamWriter(tbArchivoGenerar.Text);
+
+                for (int row = 0; row < dataGridView1.RowCount - 1; row++)
+                {
+                    int numCol = dataGridView1.Rows[row].Cells.Count;
+                    for (int col=0;col<numCol; col++)
+                    {
+                        if (col == 2 || col == 4 || col == 6) { objWriter.Write("\""); }
+                        objWriter.Write(dataGridView1.Rows[row].Cells[col].Value.ToString());
+                        if (col == 2 || col == 4 || col == 6) { objWriter.Write("\""); }
+                        objWriter.Write(",");
+                    }
+                    objWriter.WriteLine(""); // Salto de línea
+                }
+
+                objWriter.Close();
+            }
+        }
+    }
+}
